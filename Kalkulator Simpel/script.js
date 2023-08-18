9753dbf2ffdaee7302f2db6b42aa7ec520e5996e
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-08-18 14:37:54 +0700

    Update

--- Kalkulator Simpel/script.js
+++ Kalkulator Simpel/script.js
@@ -1,1 +1,8 @@
-const iniJudul = document.getElementById("judul");
+const layarInput = document.querySelector(".input");
+const angka = document.querySelectorAll(".angka");
+const reset = document.querySelectorAll(".reset");
+const hasil = document.querySelectorAll(".hasil");
+
+angka.forEach.addEventListener("click", function() {
+    alert("ok");
+});
